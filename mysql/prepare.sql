34b046ba5580643eafcea2aa91991b42fb82f9b9
kirk3110 <kirk3110@gmail.com>
2017-10-09 16:58:29 +0900

    [add] #1 セットアップスクリプトと必要なファイル群を作成
    install.shを実行することで、docker-composeで連携したmysqlサーバコンテナとpython実行コンテナが立ち上がります。
    mysqlコンテナには初期DBとユーザが作成され、pythonコンテナのpythonにはrequirements.txtに記載されたモジュールがインストールされます。

---
+++ mysql/prepare.sql
@@ -0,0 +1,16 @@
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1');
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+CREATE DATABASE gyazai DEFAULT CHARACTER SET utf8;
+USE gyazai;
+CREATE TABLE card_data
+    (
+    jan_name varchar(255),
+    eng_name varchar(255),
+    jan_text varchar(1023),
+    eng_text varchar(1023),
+    type varchar(127),
+    power_toughness varchar(15),
+    mana_cost varchar(15)
+    );
+GRANT SELECT,UPDATE,INSERT,DELETE ON `gyazai`.card_data TO gyazai_user@localhost;
