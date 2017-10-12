ae91ff7e2a533e8761aa198db2ffddc322a57b3a
kirk3110 <kirk3110@gmail.com>
2017-10-13 01:16:14 +0900

    [add] #2 カードデータを収集してDBに詰めるスクリプトを作成

---
+++ python/script/gyazai/collect_card_data_main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from tqdm import tqdm
+
+from collecter import CardDataCollecter
+from db import GyazaiDB
+
+if __name__ == '__main__':
+    # DBに接続
+    with GyazaiDB.connect(host='mysql',
+                          user='gyazai_user',
+                          password='gyazai_user_password') as gyazai_db:
+        # 全てのカード情報をデータベースに登録
+        for card_data in tqdm(CardDataCollecter.collect_all_card_data()):
+            gyazai_db.insert_card_data(card_data)
+            break
